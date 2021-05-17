11e76f3e820c6b16df5bb2204d49011219e987a9
Caroline Loppi <loppi.clg@gmail.com>
2021-05-17 12:24:07 -0300

    Add Toolbar Unit Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,72 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('molic-editor');
   });
 
-  it('should render title', () => {
+  it('should call createNewDiagram when new button is clicked', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('molic-editor app is running!');
+    const app = fixture.componentInstance;
+
+    //Spies on the createNewDiagram method
+    spyOn(app,'createNewDiagram').and.callThrough();
+    
+    //Click the newDiagram Button
+    let button = document.getElementById('newDiagram');
+    button.click();
+
+    //Check if the method createNewDiagram was called (once) after clicking the newDiagram button
+    expect(app.createNewDiagram).toHaveBeenCalled();
+    expect(app.createNewDiagram).toHaveBeenCalledTimes(1);
+
   });
+
+  it('should call exportCurrentDiagram when export button is clicked', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    //Spies on the exportCurrentDiagram method
+    spyOn(app,'exportCurrentDiagram').and.callThrough();
+    
+    //Click the exportDiagram Button
+    let button = document.getElementById('exportDiagram');
+    button.click();
+
+    //Check if the method exportCurrentDiagram was called (once) after clicking the exportDiagram button
+    expect(app.exportCurrentDiagram).toHaveBeenCalled();
+    expect(app.exportCurrentDiagram).toHaveBeenCalledTimes(1);
+
+  });
+
+  it('should call importNewDiagram when import button is clicked', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    //Spies on the importNewDiagram method
+    spyOn(app,'importNewDiagram').and.callThrough();
+
+    //Click the importDiagram Button
+    let button = document.getElementById('importDiagram');
+    button.click();
+
+    //Check if the method importNewDiagram was called (once) after clicking the importDiagram button
+    expect(app.importNewDiagram).toHaveBeenCalled();
+    expect(app.importNewDiagram).toHaveBeenCalledTimes(1);
+
+  });
+
+  it('should call takeScreenshot when screenshot button is clicked', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    //Spies on the takeScreenshot method
+    spyOn(app,'takeScreenshot').and.callThrough();
+
+    //Click the screenshot Button
+    let button = document.getElementById('screenshot');
+    button.click();
+
+    //Check if the method takeScreenshot was called (once) after clicking the screenshot button
+    expect(app.takeScreenshot).toHaveBeenCalled();
+    expect(app.takeScreenshot).toHaveBeenCalledTimes(1);
+    
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'molic-editor';
+
+  public createNewDiagram(){
+    console.log('Creates new Diagram!');
+  }
+
+  public exportCurrentDiagram(){
+    console.log('Exports current Diagram!');
+  }
+  
+  public importNewDiagram(){
+    console.log('Imports a new Diagram!');
+  }
+
+  public takeScreenshot(){
+    console.log('Takes a screenshot of the Diagram');
+  }
 }
