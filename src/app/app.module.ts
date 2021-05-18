e93542799bf3d03b0bbab2f48c9cd7da59cd673e
Caroline Loppi <loppi.clg@gmail.com>
2021-05-18 15:58:15 -0300

    Adds Bootstrap and favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DraggableElementsPanelComponent } from './draggable-elements-panel/draggable-elements-panel.component';
+import { DiagramPanelComponent } from './diagram-panel/diagram-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DraggableElementsPanelComponent,
+    DiagramPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
